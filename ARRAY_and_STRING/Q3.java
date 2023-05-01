f5789f2d1888f2c14d77348b859bfa6f3b4b3e5c
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-05-01 22:10:24 +0530

    Update Q3.java

--- ARRAY_and_STRING/Q3.java
+++ ARRAY_and_STRING/Q3.java
@@ -1,4 +1,11 @@
-
+// Write the methods with the following headers 
+// // Return the reversal of an integer. Example: reverse (456), returns 654
+// public static int reverse(int number)
+// // Return true if number is a palindrome
+// public static boolean isPalindrome(int number)
+// Use the reverse method to implement isPalindrome. A number is a palindrome if its reversal is 
+// the same as itself. Write a java program that prompts the user to enter an integer and reports 
+// whether the integer is a palindrome.
 public class Q3 {
 	
 public static int ispalendrom(int a) {
