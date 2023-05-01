1bfa9ba38b0e82d92568af21ee86e2a3edd56a7d
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-05-01 22:14:06 +0530

    Update Q9.java

--- ARRAY_and_STRING/Q9.java
+++ ARRAY_and_STRING/Q9.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
-
+// Some websites impose certain rules for passwords. 
+// Write a method that checks whether a string is a valid password. Suppose the password rules 
+// are as follows:
+//  A password must have at least eight characters.
+//  A password consists of only letters and digits.
+//  A password must contain at least two digits. 
+// Write a program that prompts the user to enter a password and displays Valid Password if the 
+// rules are followed or Invalid Password otherwise
 public class Q9 {
 //password
 	public static boolean isValidPassword(String password) {
