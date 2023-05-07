84cf9cd5ec25ca0f6ade0452cfa70b175c8d11fe
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-05-07 11:09:18 +0530

    Add files via upload

--- ARITHMETIC/Krishnmurty_Nmber.java
+++ ARITHMETIC/Krishnmurty_Nmber.java
@@ -38,7 +38,7 @@ public class Krishnmurty_Nmber {
  }
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in); 
-		System.out.println("enter a number to check it");
+		System.out.println("enter a number ");
 		int  x=sc.nextInt();
 		sc.close();
 		try {
