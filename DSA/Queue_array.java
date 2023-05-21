08214b4aa0bf4c36daa1d9d7097254fa3c25d501
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-05-21 16:22:09 +0530

    Add files via upload

---
+++ DSA/Queue_array.java
@@ -0,0 +1,58 @@
+
+public class Queue_array {
+static class queue {
+	int size;
+	static int  arr[];
+	static int rear=-1;
+	queue(int size){
+		this.size=size;
+		arr=new int[size];
+	}
+	boolean isempty() {
+		return rear==-1;
+	}
+	void add(int data) {
+		if(rear==(size-1)) {
+			System.out.println("Full queue");return;
+		}
+		rear++;
+		arr[rear]=data;
+	}
+	int remove() {
+		if(isempty()) {
+			System.out.println("Empty queue");return -1;
+		}
+		int front =arr[0];
+		for (int i=0;i<rear;i++) {
+			arr[i]=arr[i+1];
+		}
+		rear--;
+		return front;
+		
+	}
+	int  peek() {
+		if(isempty()) {
+		System.out.println("Empty queue");return -1;
+					}	
+return(arr[0]);
+	}
+	
+}
+	public static void main(String[] args) {
+		queue q=new queue(5);
+		q.add(3);
+		q.add(5);
+		q.add(6);
+		q.add(23);
+		q.add(55);
+		
+		q.add(36);
+		while(!q.isempty()) {
+			//System.out.println(q.peek()); 
+			q.remove(); 
+			
+		}
+
+	}
+
+}
