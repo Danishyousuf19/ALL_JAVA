dfb8d84a201c2ceb12c3068ab94966c57fec4678
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-06-30 15:57:37 +0530

    Add files via upload

---
+++ PATTERN/pattern_16.java
@@ -0,0 +1,16 @@
+
+public class pattern_16 {
+	void printTriangle(int n) {
+        for(int i=1;i<=n;i++) {
+        	for(int j=1;j<=i;j++) {
+        		System.out.print((char)(j+64));
+        	}
+        	System.out.println();
+        }
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
